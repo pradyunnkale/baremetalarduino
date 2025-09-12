5fd0e91d322dc92ad5e057644f5deffd9d4a189e
Pradyunn Kale <kalepradyunn@gmail.com>
2025-09-12 12:32:36 -0400

    first commit, blinking led

--- build/CMakeFiles/main.dir/main.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/main.dir/main.c.obj: \
- /home/pradyunn-kale/Projects/baremetalarduino/main.c \
- /usr/lib/gcc/avr/7.3.0/include/stdint.h /usr/lib/avr/include/stdint.h \
- /usr/lib/gcc/avr/7.3.0/include/stddef.h
